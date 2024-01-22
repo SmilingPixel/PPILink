e3b601d75d925b75212c504f89d5e0989165d704
SkyerWalkery <sky896142181@hotmail.com>
2024-01-22 16:43:33 +0800

    feat: add windows running script

---
+++ src/train.ps1
@@ -0,0 +1,5 @@
+python run.py `
+    --do_train `
+    --model_name_or_path ../models/bert-small/ `
+    --output_dir ../output `
+    --train_file ../data/cluster_1024/android_closed_issues_2011-01-01_2021-01-01_all_clean_issues_NOT_CLEANED_1024_clusters.json
