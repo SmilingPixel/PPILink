5357fcfe02aa9788a1b06dbf1957097f541a34f3
SkyerWalkery <sky896142181@hotmail.com>
2024-01-30 16:43:22 +0800

    chore: adjust some params

--- src/train.ps1
+++ src/train.ps1
@@ -2,7 +2,8 @@ python run.py `
     --do_train `
     --nlp_model_name_or_path ../models/bert-small/ `
     --code_model_name_or_path ../models/code-bert-base/ `
-    --train_batch_size 32 `
+    --train_batch_size 4 `
     --output_dir ../output `
-    --train_file ../data/small_dataset/android_closed_issues_2011-01-01_2021-01-01_cluster_range_0-6999_preprocessed.json
+    --train_file ../data/small_dataset/android_closed_issues_2011-01-01_2021-01-01_cluster_range_0-6999_preprocessed.json `
+    --num_train_epochs 1
     
