76c274601971c225c7c02f87c876d49e3b5bc531
SkyerWalkery <sky896142181@hotmail.com>
2024-01-30 16:43:08 +0800

    feat: add report and test scripts

---
+++ src/report.ps1
@@ -0,0 +1,8 @@
+python3 report/test_report.py `
+    --input ../output/... `
+    --output ../output/...
+
+python3 report/log_summary.py `
+    --input ../output/... `
+    --output ../output/...
+    
---
+++ src/test.ps1
@@ -0,0 +1,6 @@
+python run.py `
+    --do_test `
+    --main_model_name_or_path ../output/ckpt/... `
+    --eval_batch_size 64 `
+    --output_dir ../output `
+    --test_file ../data/small_dataset/android_closed_issues_2011-01-01_2021-01-01_cluster_range_8500-9999_preprocessed.json
