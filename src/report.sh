7d7067826adaad10792dfba05882459f0b985601
SkyerWalkery <sky896142181@hotmail.com>
2024-01-31 09:25:33 +0800

    chore: delete useless file

--- src/report.sh
+++
@@ -1,8 +0,0 @@
-python3 report/test_report.py \
-    --input ../output/... \
-    --output ../output/...
-
-python3 report/log_summary.py \
-    --input ../output/... \
-    --output ../output/...
-    
